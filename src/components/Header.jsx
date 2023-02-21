dacb162b108f7c4226b4982eacad36eb761c8864
Azizkhan Aliakbar <azizxan999@gmail.com>
2023-02-21 22:18:33 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="http://www.rw-designer.com/icon-image/22723-256x256x32.png" />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://r4.wallpaperflare.com/wallpaper/285/701/304/anime-anime-girls-seymour-ningguang-genshin-impact-genshin-impact-hd-wallpaper-ee9c1bfe5f7632d8907a662a4b7adb8a.jpg" />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
