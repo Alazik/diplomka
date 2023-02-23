98c3d53a0e2f0d306d383a691570211e6176c96a
Azizkhan Aliakbar <azizxan999@gmail.com>
2023-02-23 17:47:34 +0600

    route

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from "./Profile.module.css";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://r4.wallpaperflare.com/wallpaper/285/701/304/anime-anime-girls-seymour-ningguang-genshin-impact-genshin-impact-hd-wallpaper-ee9c1bfe5f7632d8907a662a4b7adb8a.jpg" />
             </div>
