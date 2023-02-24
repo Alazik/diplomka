9724b426205cc68cc3fdf2c6b1f332b283ff7c71
Azizkhan Aliakbar <azizxan999@gmail.com>
2023-02-24 16:29:06 +0600

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,27 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + " " + s.active}>
+                    Nurik
+                </div>
+                <div className={s.dialog}>
+                    Sula
+                </div>
+                <div className={s.dialog}>
+                    Islam
+                </div>
+                <div className={s.dialog}>
+                    Almaznik
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Siiuuuuuuuuuuuuuuu</div>
+                <div className={s.message}>Я просто обожаю 3d хуйню</div>
+                <div className={s.message}>Салам бро, как ты?</div>
+                <div className={s.message}>Эщс</div>
+            </div>
         </div>
     );
 };
