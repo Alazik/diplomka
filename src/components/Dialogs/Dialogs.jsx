b77f961de9b43c3a5cfc298db17b8869f785e703
Azizkhan Aliakbar <azizxan999@gmail.com>
2023-02-27 22:33:16 +0600

    map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,34 +17,44 @@ const Message = (props) => {
     );
 };
 
-let dialogsData = [
+let dialogs = [
     { id: 1, name: "Nurik" },
     { id: 2, name: "Sula" },
     { id: 3, name: "Islam" },
     { id: 4, name: "Almaznik" },
 ];
 
-let messagesData = [
+let dialogsElements = dialogs .map(d => <DialogItem name={d.name} id={d.id} /> );
+
+let messages = [
     { id: 1, message: "Siiuuuuuuuuuuuuuuu" },
     { id: 2, message: "Я просто обожаю 3d хуйню" },
     { id: 3, message: "Салам бро, как ты?" },
     { id: 4, message: "Эщс" },
 ];
 
+let messagesElements = messages .map(m => <Message message={m.message} />)
+
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
-            <div className={s.dialogsItem}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+            <div className={s.dialogsItems}>
+
+                {dialogsElements}
+
+                {/* <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
                 <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
                 <DialogItem name="Islam" id="3" />
-                <DialogItem name="Almaznik" id="4" />
+                <DialogItem name="Almaznik" id="4" /> */}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} />
-                <Message message={messagesData[3].message} />
+
+                {messagesElements}
+
+                {/* <Message message={messages[0].message} />
+                <Message message={messages[1].message} />
+                <Message message={messages[2].message} />
+                <Message message={messages[3].message} /> */}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-let postsData = [
+let posts = [
     {id: 1, message: "Hi, how are you?", likesCount: "1"},
     {id: 2, message: "Salam bro", likesCount: "12"},
     {id: 3, message: "Чуваки сверху пидоры", likesCount: "100"}
 ];
+
+let postsElement = posts.map (p => <Post message={p.message} likesCount={p.likesCount} />)
 
 const MyPosts = () => {
     return (
@@ -21,9 +23,12 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
-                <Post message={postsData[2].message} likesCount={postsData[2].likesCount} />
+                
+                {postsElement}
+                
+                {/* <Post message={posts[0].message} likesCount={posts[0].likesCount} />
+                <Post message={posts[1].message} likesCount={posts[1].likesCount} />
+                <Post message={posts[2].message} likesCount={posts[2].likesCount} /> */}
             </div>
         </div>
 
