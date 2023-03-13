9be1a1ec1078e7da1517a6168367b380da79df01
Azizkhan Aliakbar <azizxan999@gmail.com>
2023-03-13 22:21:57 +0600

    bind, store, OOP

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { rerenderEntireTree } from "./render";
-import state from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/state";
 
-rerenderEntireTree(state);
+const root = ReactDOM.createRoot(document.getElementById("root")); //Вывел это строку из функции rerenderEntireTree чтобы не пропал фокус с textarea, после ввода каждого символа актуальной на версии реакта объявляется переменная root, через которую уже позже рендериться приложение
+
+let rerenderEntireTree = () => {
+  root.render(
+    <BrowserRouter>
+      <React.StrictMode>
+        <App
+          state={store._state}
+          addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
+        />
+      </React.StrictMode>
+    </BrowserRouter>
+  );
+};
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,50 +1,61 @@
-import { rerenderEntireTree } from "../render";
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: "1" },
+        { id: 2, message: "Salam bro", likesCount: "12" },
+        { id: 3, message: "Чуваки сверху пидоры", likesCount: "100" },
+      ],
+      newPostText: "",
+    },
 
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi, how are you?", likesCount: "1" },
-      { id: 2, message: "Salam bro", likesCount: "12" },
-      { id: 3, message: "Чуваки сверху пидоры", likesCount: "100" },
-    ],
-    newPostText: ""
+    dialogsPage: {
+      messages: [
+        { id: 1, message: "Siiuuuuuuuuuuuuuuu" },
+        { id: 2, message: "Я просто обожаю 3d хуйню" },
+        { id: 3, message: "Салам бро, как ты?" },
+        { id: 4, message: "Эщс" },
+      ],
+      dialogs: [
+        { id: 1, name: "Nurik", avatar: "" },
+        { id: 2, name: "Sula" },
+        { id: 3, name: "Islam" },
+        { id: 4, name: "Almaznik" },
+      ],
+    },
+
+    sidebar: {},
   },
 
-  dialogsPage: {
-    messages: [
-      { id: 1, message: "Siiuuuuuuuuuuuuuuu" },
-      { id: 2, message: "Я просто обожаю 3d хуйню" },
-      { id: 3, message: "Салам бро, как ты?" },
-      { id: 4, message: "Эщс" },
-    ],
-    dialogs: [
-      { id: 1, name: "Nurik", avatar: "" },
-      { id: 2, name: "Sula" },
-      { id: 3, name: "Islam" },
-      { id: 4, name: "Almaznik" },
-    ],
+  getState() {
+    return this._state;
   },
 
-  sidebar: {},
+  _callSubscriber() {
+    console.log("State has been changed");
+  },
+
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber();
+  },
+
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber();
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 };
 
-window.state = state;
+export default store;
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  };
-
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export default state;
+window.store = store;
